e58176b5a26f01b8140aa5e8d8ef13ed8c4ec2e3
Lubin <Lubin@Lubins-MacBook-Pro.local>
2017-11-23 17:49:14 +0800

    Changed version in nxt-default.properties and nxt.java to 1.11.9 to suppress blacklisting.
    changed typo in one ip address in defaultpeers and wellknownpeers.

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -51,7 +51,7 @@ import java.util.Properties;
 
 public final class Nxt { //only public class in a Java file must also have the main method
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.11.9";
     public static final String APPLICATION = "Taelium";
 
     private static volatile Time time = new Time.EpochTime();
